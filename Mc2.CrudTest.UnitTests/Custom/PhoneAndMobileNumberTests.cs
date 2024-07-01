6f613456f66dd15bc13f7193ad5146ea84388640
Afshin Razaghi <neo1.afshin@gmail.com>
2024-07-01 16:42:22 +0330

    PhoneAndMobileNumberTests

---
+++ Mc2.CrudTest.UnitTests/Custom/PhoneAndMobileNumberTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Mc2.CrudTest.Presentation.Domain.ValueObjects;
+using Mc2.CrudTest.Presentation.Domain.ValueObjects.Validators;
+using PhoneNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.UnitTests.Custom
+{
+    public class PhoneAndMobileNumberTests
+    {
+        [Theory]
+        [InlineData("+16156381234")]
+        [InlineData("+989121234567")]
+        public void CheckPhoneNumber_WhenCalledWithMobile_ReturnsTrue(string mobileNumber)
+        {
+            // Arrange
+            var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+            var parsedPhoneNumber = phoneNumberUtil.Parse(mobileNumber, null);
+            // Act
+            var res = phoneNumberUtil.GetNumberType(parsedPhoneNumber);
+
+            // Assert
+            res.Should().BeOneOf(PhoneNumberType.FIXED_LINE_OR_MOBILE, PhoneNumberType.MOBILE);
+                
+        }
+
+
+        [Fact]
+        public void CheckPhoneNumber_WhenCalledWithPhoneNumber_ReturnsFalse()
+        {
+            // Arrange
+            string myNumber = "+982188776655";
+            var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+            var parsedPhoneNumber = phoneNumberUtil.Parse(myNumber, "IR");
+            // Act
+            var res = phoneNumberUtil.GetNumberType(parsedPhoneNumber);
+
+            // Assert
+            res.Should().NotBe(PhoneNumberType.MOBILE);
+        }
+    }
+}
